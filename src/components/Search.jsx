5a6b84d6a58fc0f1f6872ad40d063ced93ba691e
apparatus <176843282+gamius32@users.noreply.github.com>
2025-03-07 17:56:15 +0100

    Added search component

---
+++ src/components/Search.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Search = (props) => {
+  return (
+    <div className='text-white text-3xl'>
+        {props.searchTerm}
+    </div>
+  )
+}
+
+export default Search
