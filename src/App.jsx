fb10f970b1637316c1f170e49d98141d49ac0376
apparatus <176843282+gamius32@users.noreply.github.com>
2025-03-07 17:43:27 +0100

    Added assets to public and added some stuff to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,17 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
+    <main>
+      <div className='pattern'/>
+
+      <div className='wrapper'>
+        <header>
+          <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle</h1>
+
+          <p>Search</p>
+        </header>
+      </div>
+    </main >
   )
 }
 
