ced2ee6d4f16fe28fce8e84bc5be3de41a5b871c
apparatus <176843282+gamius32@users.noreply.github.com>
2025-03-10 13:52:45 +0100

    Finished header

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import Search from './components/Search'
 
 const App = () => {
-  const [searchTerm, setSearchTerm] = useState('I am the one who knocks');
+  const [searchTerm, setSearchTerm] = useState('');
 
   return (
     <main>
@@ -14,6 +14,7 @@ const App = () => {
           <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle</h1>
         </header>
         <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        <h1 className='text-white'>{searchTerm}</h1>
       </div>
     </main >
   )
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
-const Search = (props) => {
+const Search = ({searchTerm, setSearchTerm}) => {
   return (
-    <div className='text-white text-3xl'>
-        {props.searchTerm}
+    <div className='search'>
+        <div>
+          <img src="search.svg" alt="search" />
+          <input type="text" 
+          placeholder='Search through thousands of movies'
+          value={searchTerm}
+          onChange={(event) => setSearchTerm(event.target.value)}/>
+        </div>
     </div>
   )
 }
